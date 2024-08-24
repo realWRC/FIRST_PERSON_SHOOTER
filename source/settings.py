56bd307140804ae5aa8a26c44ef07c0af305479a
Wongani Chulu <wonganichulu@gmail.com>
2024-08-24 21:14:00 +0200

    Added dynamic window scaling on initalisation

--- source/settings.py
+++ source/settings.py
@@ -1,3 +1,12 @@
+import pygame as pg
+
+def calculateResolution(scale=1):
+    """Calculates screen resolution for dynamic screen sizing"""
+    pg.init()
+    screen = pg.display.Info()
+    pg.quit()
+    return int(screen.current_w * scale), int(screen.current_h * scale)
+
 #SCREEN RESOLUTION
-RES = WIDTH, HEIGHT = 1600, 900
+RES = WIDTH, HEIGHT = calculateResolution(0.9)
 FPS = 60
