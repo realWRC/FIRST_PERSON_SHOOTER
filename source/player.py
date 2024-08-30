c67d2fa15b4f91fde727998ffbcb6b275fc3566a
Wongani Chulu <wonganichulu@gmail.com>
2024-08-30 17:03:50 +0200

    Refactored Player

--- source/player.py
+++ source/player.py
@@ -2,7 +2,6 @@ from source.settings import *
 import pygame as pg
 import math
 
-
 class Player:
     """Defines the Player"""
 
@@ -26,27 +25,29 @@ class Player:
         speedSin = speed * angleSin
         speedCos = speed * angleCos
 
-        keys = pg.key.get_pressed()
-        if keys[pg.K_w]:
+        buttons = pg.key.get_pressed()
+        if buttons[pg.K_w]:
             px += speedCos
             py += speedSin
-        if keys[pg.K_s]:
+        if buttons[pg.K_s]:
             px += -speedCos
             py += -speedSin
-        if keys[pg.K_a]:
+        if buttons[pg.K_a]:
             px += speedSin
             py += -speedCos
-        if keys[pg.K_d]:
+        if buttons[pg.K_d]:
             px += -speedSin
             py += speedCos
 
         self.wallCollision(px, py)
 
-        if ENABLE_KEY_ROTATION is True:
-            if keys[pg.K_LEFT]:
+        if ENABLE_KEY_ROTATION == True:
+            if buttons[pg.K_LEFT]:
                 self.angle -= PLAYER_ROTATION_SPEED * self.game.deltaTime
-            if keys[pg.K_RIGHT]:
+            if buttons[pg.K_RIGHT]:
                 self.angle += PLAYER_ROTATION_SPEED * self.game.deltaTime
+
+        self.angle %= math.tau
 
     @property
     def position(self):
@@ -60,27 +61,27 @@ class Player:
 
     def getWallBounds(self, x, y):
         """Returns position if there is no wall"""
-        if (x, y) not in self.game.map.gameWorld:
-            return (x, y)
+        return (x, y) not in self.game.map.gameWorld
 
     def wallCollision(self, px, py):
         """Collision detection logic for player and walls"""
-        playerScale = PLAYERSCALE / self.game.deltaTime
-        if self.getWallBounds(int(self.x + (px * playerScale)), int(self.y)):
+        playerScale = PLAYER_SIZE_SCALE / self.game.deltaTime
+        if self.getWallBounds(int(self.x + px * playerScale), int(self.y)):
             self.x += px
-        if self.getWallBounds(int(self.x), int(self.y + (py * playerScale))):
+        if self.getWallBounds(int(self.x), int(self.y + py * playerScale)):
             self.y += py
 
     def mouseControl(self):
         """Controls Player object with mouse"""
         mx, my = pg.mouse.get_pos()
-        if mx < MOUSE_LEFT_BORDER or mx > MOUSE_RIGHT_BORDER:
-            pg.mouse.set_pos([HALFWIDTH, HALFHEIGHT])
+        if mx < MOUSES_LEFT_BORDER or mx > MOUSES_RIGHT_BORDER:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
         self.relativePosition = pg.mouse.get_rel()[0]
         self.relativePosition = max(
-            -MOUSE_MAX_REL, min(MOUSE_MAX_REL, self.relativePosition)
+                -MOUSES_MAXUMUM_RELAT,
+                min(MOUSES_MAXUMUM_RELAT, self.relativePosition)
         )
-        self.angle += self.relativePosition * MOUSESENTITIVITY *\
+        self.angle += self.relativePosition * MOUSES_SENSITIVITY *\
             self.game.deltaTime
 
     def testDraw(self):
@@ -102,7 +103,5 @@ class Player:
         )
 
     def update(self):
-        """Updates state of Player in core game loop"""
         self.movement()
-        if ENABLE_MOUSE is True:
-            self.mouseControl()
+        self.mouseControl()
