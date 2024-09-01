8bf2a7ea23a68b675d3fb3a22a9ea5b67722293d
Wongani Chulu <wonganichulu@gmail.com>
2024-09-01 16:14:38 +0200

    Fixed pathfinding bug

--- source/pathfinding.py
+++ source/pathfinding.py
@@ -10,16 +10,14 @@ class PathFinding:
         self.graph = {}
         self.constructGraph()
 
-    @lru_cache
-    def getRoute(self, start, goal):
-        self.visited = self.breadFirstSearch(start, goal, self.graph)
-        path = [goal]
-        step = self.visited.get(goal, start)
+    def constructGraph(self):
+        for y, row in enumerate(self.map):
+            for x, col in enumerate(row):
+                if not col:
+                    self.graph[(x, y)] = self.graph.get((x, y), []) + self.getNextTile(x, y)
 
-        while step and step != start:
-            path.append(step)
-            step = self.visited[step]
-        return path[-1]
+    def getNextTile(self, x, y):
+        return [(x + dx, y + dy) for dx, dy in self.routes if (x + dx, y + dy) not in self.game.map.gameWorld]
 
     def breadFirstSearch(self, start, goal, graph):
         queue = deque([start])
@@ -35,11 +33,13 @@ class PathFinding:
                     visitedTiles[nextTile] = currentTile
         return visitedTiles
 
-    def getNextTile(self, x, y):
-        return [(x + dx, y + dy) for dx, dy in self.routes if (x + dx, y + dy) not in self.game.map.gameWorld]
+    @lru_cache
+    def getRoute(self, start, goal):
+        self.visited = self.breadFirstSearch(start, goal, self.graph)
+        path = [goal]
+        step = self.visited.get(goal, start)
 
-    def constructGraph(self):
-        for y, row in enumerate(self.map):
-            for x, col in enumerate(row):
-                if not col:
-                    self.graph[(x, y)] = self.graph.get((x, y), []) + self.getNextTile(x, y)
+        while step and step != start:
+            path.append(step)
+            step = self.visited[step]
+        return path[-1]
