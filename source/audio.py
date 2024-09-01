36514c924e39c69c387a972684b89e7d1b1bf4f5
Wongani Chulu <wonganichulu@gmail.com>
2024-09-01 15:04:12 +0200

    Added settings for pathfinding

--- source/audio.py
+++ source/audio.py
@@ -8,4 +8,8 @@ class Audio:
         """Initialises all audio resources"""
         pg.mixer.init()
         self.path = 'resources/audio/'
-        self.shotgun = pg.mixer.Sound(self.path + 'shotgun_shot.mp3')
+        self.shotgun = pg.mixer.Sound(self.path + 'shotgun_shot.wav')
+        self.enemyPain = pg.mixer.Sound(self.path + 'enemy_pain.wav')
+        self.enemyDeath = pg.mixer.Sound(self.path + 'enemy_death.wav')
+        self.enemyFire = pg.mixer.Sound(self.path + 'enemy_attack.wav')
+        self.playerPain = pg.mixer.Sound(self.path + 'player_pain.wav')
